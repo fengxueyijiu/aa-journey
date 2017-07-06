eef45d45f9a0ff9fa52baf09b1c63e6f55847d86
codeedoc <710987071@qq.com>
2017-07-06 13:04:30 +0800

    i

--- server/controllers/cat.js
+++ server/controllers/cat.js
@@ -1,3 +1,14 @@
-exports.add = function(req,res){
-  res.send('controllers/cat.js...')
+let Cat = require('../models/cat');
+
+exports.add =function(req,res){
+  let _category = req.body;
+  // name=?
+  let category = new Cat(_category);
+  category.save(function(err,category){
+    if(err) return res.status(403).json({err,msg:'添加失败请重试'});
+    res.json({
+      msg:'分类添加成功',
+      category
+    })
+  })
 }
--- server/index.js
+++ server/index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 let routes = require('./routes');
+var mongoose = require('mongoose');
 const app=express();
 routes(app);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-
+mongoose.Promise= global.Promise;
+let dbURL = 'mongodb://localhost:27017/aa-journey';
+mongoose.connect(dbURL);
+var db=mongoose.connection;
+db.once('open',function(){
+  console.log('mongodb connect success')
+})
 app.listen(3000,function(){
   console.log('running on port 3000...')
 })
